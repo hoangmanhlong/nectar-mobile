4c26eed035b76d287b657346e97f62f1b26f81ac
LongHM <hoang.manh.long@zensho.com>
2024-10-30 09:46:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nectar
+
+Nectar online groceries
---
+++ android/app/src/main/kotlin/com/flutter/nectar/nectar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter.nectar.nectar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
